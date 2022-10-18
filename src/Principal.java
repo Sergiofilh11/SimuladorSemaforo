e8e81df1ec658279b5b5badc9a3421e09bcd98c1
Sérgio Filho <fragasergio23@gmail.com>
2022-10-18 10:55:13 -0300

    Implementing finish feature

--- src/Principal.java
+++ src/Principal.java
@@ -1,13 +1,33 @@
+import java.util.Scanner;
 
 public class Principal {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		ThreadSemaforo semaforo = new ThreadSemaforo();
 		
-		for (int i=0; i<10;i++) {
+		String isContinue;
+		Scanner scanner;
+		
+		for(int i=0; i<400; i++) {
 			System.out.println(semaforo.getCor());
 			semaforo.esperaCorMudar();
+			
+			if(i==2) {
+				System.out.println("Deseja continuar? (s/n)");
+				
+				scanner = new Scanner(System.in);
+				isContinue = scanner.nextLine();
+				
+				if(isContinue.equals("s")) {
+					i=0;
+					semaforo.esperaCorMudar();
+				}
+				else if(isContinue.equals("n")) {
+					System.out.println("Programa finalizado!!");
+					break;
+				}
+			}
 		}
 	}
 }
